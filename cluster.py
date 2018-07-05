814118d06d9fd9781f15506b42bb480afbe8d2d8
kobiweisberg <40637124+kobiweisberg@users.noreply.github.com>
2018-07-05 12:54:34 +0300

    S

--- cluster.py
+++ cluster.py
@@ -1,9 +1,15 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import normalize
 import numpy as np
+from kmeans_cosine_distance import KMeans as KMeans_cos_dist
 
 def kmeans(vecs,k):
     normed_vecs = normalize(vecs, axis=1, norm='l2')
     kmeans_class = KMeans(n_clusters=k, init='random',random_state=4)
     clusters = kmeans_class.fit_predict(normed_vecs)
     return clusters,kmeans_class
+
+def kmeans_cosine_dist(vecs,k):
+    kmeans_class = KMeans_cos_dist(k,list(vecs))
+    kmeans_class.main_loop()
+    return kmeans_class.indexes,kmeans_class
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import vectorizer as vr
 import cluster as clst
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import time
+t = time.localtime()
+timestamp = time.strftime('%b_%d_%Y_%H%M', t)
 np.random.seed(4)
 newsgroups_train = fetch_20newsgroups(subset='train')
 
@@ -24,10 +27,10 @@ if __name__=='__main__':
     ###################
     # parameters
     ###################
-    min_df = 1e-06#1e-10#1e-06
-    max_df = 0.05#1#0.05
+    min_df = 1e-6#1e-10
+    max_df = 0.05#1
     k = 40
-    files = emails#[:5]
+    files = emails[:1000]
     ###################
     # algo
     ###################
@@ -47,12 +50,14 @@ if __name__=='__main__':
     '''
     ######TODO: not good, only euclid dist
     print('compute kmeans clusters')
-    kmns_clusters,kmns_class = clst.kmeans(BOW,k)
+    kmns_clusters,kmns_class = clst.kmeans_cosine_dist(BOW,k)
     print('compute confusion matrix')
-    conf_mat = confusion_matrix(labels, kmns_clusters)
+    conf_mat = confusion_matrix(labels[:1000], kmns_clusters)
     plt.figure(1)
     plt.imshow(conf_mat)
     plt.show()
+    with open(timestamp+'_kmeans.pkl','wb') as wf:
+        pickle.dump(conf_mat,wf)
 
     pass
 
