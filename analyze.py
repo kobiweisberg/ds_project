1caeae867f34d1395477a00c81fe0437fc03fffc
kobiweisberg <40637124+kobiweisberg@users.noreply.github.com>
2018-07-08 18:08:03 +0300

    add hirarchical analysis and savefigs

--- analyze.py
+++ analyze.py
@@ -1,5 +1,19 @@
 import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+def evaluate_many2one(mat,num_of_labels):
+    mat = mat[:num_of_labels,:]
+    correct_pred = np.zeros(num_of_labels)
+    for col in mat.T:
+        correct_pred[np.argmax(col)] +=np.max(col)
 
-def evaluate_many2one(mat):
-    correct_num = np.amax(mat,axis=0)
-    pass
+    return correct_pred/np.sum(mat,axis=1)
+
+
+if __name__=='__main__':
+    with open('results/','rb') as rf:
+        mat = pickle.load(rf)
+    ret = evaluate_many2one(mat,20)
+    plt.figure(1)
+    plt.plot(range(1,21),ret,'*')
+    plt.show()
--- cluster.py
+++ cluster.py
@@ -2,7 +2,7 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import normalize
 import numpy as np
 from kmeans_cosine_distance import KMeans as KMeans_cos_dist
-
+from sklearn.cluster import AgglomerativeClustering
 def kmeans(vecs,k):
     normed_vecs = normalize(vecs, axis=1, norm='l2')
     kmeans_class = KMeans(n_clusters=k, init='random',random_state=4)
@@ -12,4 +12,11 @@ def kmeans(vecs,k):
 def kmeans_cosine_dist(vecs,k):
     kmeans_class = KMeans_cos_dist(k,list(vecs))
     kmeans_class.main_loop()
-    return kmeans_class.indexes,kmeans_class
+    return kmeans_class.vectors_indexes,kmeans_class
+
+def hirarchical(vecs,k,aff = 'cosine',link='complete'):
+    # Affinity = {“euclidean”, “l1”, “l2”, “manhattan”, “cosine”}
+    # Linkage = {“ward”, “complete”, “average”}
+    Hclustering = AgglomerativeClustering(n_clusters=k,affinity=aff, linkage=link)
+    clusters = Hclustering.fit_predict(vecs)
+    return clusters,Hclustering
