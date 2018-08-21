eaa5462544e3c3191cfd5515df7bec0f8a8f2582
kobiweisberg <40637124+kobiweisberg@users.noreply.github.com>
2018-08-21 12:32:10 +0300

    add linkage matrix, save full presiction and recall

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
-
+from utils import load_results
 
 def evaluate_many2one(mat, num_of_labels):
     mat = mat[:num_of_labels,:]
@@ -62,20 +62,48 @@ def conf_mat2scores(cm):
     recall = np.nan_to_num(recall, 0)
     return accuracy,precision,recall
 
+
+def calc_linkage_table(clustars):
+    N = len(clustars)
+    table = np.zeros((N,N))
+    for i in range(N):
+        for j in range(N):
+           table[i,j] = int(clustars[i] == clustars[j])
+    return table
+
 class Results:
-    def __init__(self,acc, precision, recall, conf_mat, labels_conf_mat):
+    def __init__(self,acc, precision, recall, conf_mat, labels_conf_mat, linkage_table):
         self.acc = acc
         self.precision = precision
         self.recall = recall
         self.conf_mat = conf_mat
         self.labels_conf_mat = labels_conf_mat
+        self.linkage_table =  linkage_table
     def __str__(self):
         return 'acc = {}, precision = {}, recall = {}'.format(self.acc,np.mean(self.precision),np.mean(self.recall))
+    def get_list(self):
+        return [self.acc, np.mean(self.precision), np.mean(self.recall)]
+    def get_list_full_precision(self):
+        return list(self.precision)
+    def get_list_full_recall(self):
+        return list(self.recall)
 def analyze_clustering(labels, clusters, number_of_labels):
     conf_mat = confusion_matrix(labels, clusters)
+    linkage_table = calc_linkage_table(clusters)
     labels_conf_mat = evaluate_many2one_conf_mat(conf_mat, number_of_labels)
     acc, precision, recall = conf_mat2scores(labels_conf_mat)
-    return Results(acc, precision, recall, conf_mat, labels_conf_mat)
+    return Results(acc, precision, recall, conf_mat, labels_conf_mat, linkage_table)
+
+
+def calc_total_linkage_matrix(number_of_docs):
+    results = load_results()
+    all_link_mat = [res['results'].linkage_table for res in results if res['params'].get_list()[-1] == number_of_docs]
+    total_link = np.zeros((number_of_docs, number_of_docs))
+    for link_mat in all_link_mat:
+        total_link = total_link + link_mat
+    return total_link
+
+
 if __name__=='__main__':
     import matplotlib.pyplot as plt
     import pickle
@@ -85,5 +113,15 @@ if __name__=='__main__':
     plt.figure(1)
     plt.plot(range(1,21),ret,'*')
     plt.show()'''
-    a = np.array([[38,2,200],[47,53,4]])
-    print(evaluate_many2one_conf_mat(a,2))
+    #a = np.array([[38,2,200],[47,53,4]])
+    #print(evaluate_many2one_conf_mat(a,2))
+    #a = np.array([1, 2, 1, 1, 0, 1, 0])
+    #t = calc_linkage_table(a)
+    #print(t)
+    results = load_results()
+    all_link_mat = [res['results'].linkage_table for res in results if res['params'].get_list()[-1]==250]
+    total_link = np.zeros((250,250))
+    for link_mat in all_link_mat:
+        total_link = total_link + link_mat
+
+    print(total_link)
