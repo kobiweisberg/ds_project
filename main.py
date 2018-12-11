93b995aab609aa8949e0e96ef0a22b1e7877155e
kobiweisberg <kobyavi@gmail.com>
2018-12-11 11:54:01 +0200

    Merge branch 'master' of https://github.com/kobiweisberg/ds_project
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -14,14 +14,16 @@ import cluster as clst
 import analyze as anlz
 import warnings
 import argparse
-from LM_vectorizer import batchify,plot_tsne,get_docs_repr
+from LM_vectorizer import batchify, plot_tsne, get_docs_repr
 
 import torch
 from torch.autograd import Variable
 
-#import data
+# import data
 from dataloader import *
-#from LM_hagai import repackage_hidden
+
+
+# from LM_hagai import repackage_hidden
 
 def repackage_hidden(h):
     """Wraps hidden states in new Tensors, to detach them from their history."""
@@ -29,6 +31,7 @@ def repackage_hidden(h):
         return h.detach()
     else:
         return tuple(repackage_hidden(v) for v in h)
+
 
 parser = argparse.ArgumentParser(description='Language Model')
 
@@ -43,6 +46,8 @@ parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
+parser.add_argument('--dellink', action='store_true',
+                    help='delete old linkage table')
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
@@ -53,7 +58,7 @@ if torch.cuda.is_available():
 device = torch.device("cuda" if args.cuda else "cpu")
 
 with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f,map_location='cpu').to(device)
+    model = torch.load(f, map_location='cpu').to(device)
 model.eval()
 
 corpus = Dataloader(args)
@@ -62,25 +67,25 @@ num_of_documents = len(corpus.only_encoded_docs)
 labels = corpus.labels
 pp_docs = [' '.join([corpus.decoder[str(w)] for w in doc]) for doc in corpus.only_encoded_docs]
 emails = pp_docs
-#emails = corpus.raw_data
-#labels = corpus.raw_labels
+# emails = corpus.raw_data
+# labels = corpus.raw_labels
 
 for example_idx in range(20):
-    with open('/home/lab/taitelh/PycharmProjects/ds_project/results_hagai/examples/' + str(example_idx) + '.txt','w') as wf:
+    with open('../../examples/' + str(example_idx) + '.txt', 'w') as wf:
         wf.write('Label (pp): %s\n' % corpus.target_names[corpus.labels[example_idx]])
         wf.write('Label (raw): %s\n' % corpus.target_names[corpus.raw_labels[corpus.encoded_docs[example_idx][0]]])
         wf.write('\n------------------------------\n')
         wf.write(pp_docs[example_idx])
         wf.write('\n------------------------------\n')
         wf.write(corpus.raw_data[corpus.encoded_docs[example_idx][0]])
-number_of_labels = 20 #TODO magic number
-max_df = 1.#0.05
-min_df = 0.#1e-4
-all_k = [20 , 50, 200]
+number_of_labels = 20  # TODO magic number
+max_df = 1.  # 0.05
+min_df = 0.  # 1e-4
+all_k = [20, 50, 200]
 all_vect = [vect_tfidf, vect_w2v, vect_bow]
 assert (len(emails) == len(labels))
 print(len(emails))
-prarameters = [Params(vect_bow, clust_kneams, aff_euclidean, link_ward, min_df, max_df, 20, num_of_documents)]
+prarameters = [Params(vect_w2v, clust_kneams, aff_euclidean, link_ward, min_df, max_df, 20, num_of_documents)]
 '''for vect, k in itertools.product(all_vect,all_k):
     prarameters = prarameters + \
     [
@@ -101,83 +106,80 @@ files = os.listdir(os.path.curdir)
 for file in files:
     if file.startswith('linkage_table'):
         warnings.warn('warning: csv file already exists')
-        print('delete %s?' % file)
-        ans = input().lower()
-        print(ans)
-        if(ans == 'y'):
+        ans = args.dellink
+        if (ans):
             os.remove(file)
             print('%s deleted!' % file)
 
-
-#params
-
+# params
 
 
-
-for idx,param in enumerate(prarameters):
+for idx, param in enumerate(prarameters):
     try:
-        #setup
-        #t = time.localtime()
-        #timestamp = time.strftime('%b_%d_%Y_%H%M', t)
-        #if not os.path.exists('results'):
+        # setup
+        # t = time.localtime()
+        # timestamp = time.strftime('%b_%d_%Y_%H%M', t)
+        # if not os.path.exists('results'):
         #    os.mkdir('results')
-        #if not os.path.exists('results/' + timestamp):
+        # if not os.path.exists('results/' + timestamp):
         #    os.mkdir('results/' + timestamp)
 
         np.random.seed(4)
-        print('iter {}/{}'.format(idx,len(prarameters)))
+        print('iter {}/{}'.format(idx, len(prarameters)))
 
         emails = emails[:param.max_num]
 
-        #preprocess
-        #emails = [pp.clean_text(e) for e in emails]
+        # preprocess
+        # emails = [pp.clean_text(e) for e in emails]
         print('vectorizing...')
         ##vectorizing
-        if(param.vectorizing == vect_bow):
-            #BOW
-            cv,tokenized_emails = vr.tokenizer(emails,param.min_df,param.max_df)
-            #{k: v for k, v in zip(cv.get_feature_names(), sample_vec.toarray()[0]) if v > 0}
+        if (param.vectorizing == vect_bow):
+            # BOW
+            cv, tokenized_emails = vr.tokenizer(emails, param.min_df, param.max_df)
+            # {k: v for k, v in zip(cv.get_feature_names(), sample_vec.toarray()[0]) if v > 0}
             # to get vocabulary names use: cv.get_feature_names()
             voc_names = cv.get_feature_names()
-            #to get BOW for each mail use: tokenized_emails.toarray()
+            # to get BOW for each mail use: tokenized_emails.toarray()
             emails_representation = tokenized_emails.toarray()
-        elif(param.vectorizing == vect_tfidf):
-            #TF-IDF
-            tf_idf = vr.tf_idf(emails,param.min_df,param.max_df)
+        elif (param.vectorizing == vect_tfidf):
+            # TF-IDF
+            tf_idf = vr.tf_idf(emails, param.min_df, param.max_df)
             emails_representation = tf_idf.toarray()
-        elif(param.vectorizing == vect_w2v):
-            #Word2Vec
+        elif (param.vectorizing == vect_w2v):
+            # Word2Vec
             emails_representation = vr.BOW_w2v(emails)
         elif param.vectorizing == vect_LM:
             data = batchify(corpus.only_encoded_docs, 1, device)
             emails_representation = get_docs_repr(model, data)
-        #anlz.tsne_plot(emails_representation, labels, args.seed)
+        # anlz.tsne_plot(emails_representation, labels, args.seed)
         print('clustering...')
         ##clustering
-        if(param.clustering == clust_kneams):
-            if(param.affine == 'cosine'):
-                #k-means cosine dist
-                clusters,kmns_class = clst.kmeans_cosine_dist(emails_representation,param.k)
-            elif(param.affine == 'euclidean'):#k-means euclidean
-                clusters,kmns_class = clst.kmeans(emails_representation,param.k)
+        if (param.clustering == clust_kneams):
+            if (param.affine == 'cosine'):
+                # k-means cosine dist
+                clusters, kmns_class = clst.kmeans_cosine_dist(emails_representation, param.k)
+            elif (param.affine == 'euclidean'):  # k-means euclidean
+                clusters, kmns_class = clst.kmeans(emails_representation, param.k)
+                print('kmeans sum of distanses = %f' % kmns_class.inertia_)
             else:
                 raise ValueError('kmenas is not supported with affinity: ' + param.affine)
-        elif(param.clustering == clust_hirarchical):
-            #hirarchical
-            (clusters, _) = clst.hirarchical(emails_representation, param.k,aff=param.affine,link=param.linkage)
+        elif (param.clustering == clust_hirarchical):
+            # hirarchical
+            (clusters, _) = clst.hirarchical(emails_representation, param.k, aff=param.affine, link=param.linkage)
         else:
             raise ValueError('clustering is not supported with: ' + param.clustering)
-        #analyze
+        # analyze
 
         random_clst = np.random.randint(0, param.k, param.max_num)
-        results = anlz.analyze_clustering(labels[:param.max_num],clusters,number_of_labels)
+        results = anlz.analyze_clustering(labels[:param.max_num], clusters, number_of_labels)
         results_rand = anlz.analyze_clustering(labels[:param.max_num], random_clst, number_of_labels)
-        results_super_class = anlz.analyze_clustering(corpus.super_class_labels[:param.max_num],clusters,number_of_labels)
-        #print('accuracy: ', results.acc)
-        #print('accuracy random labeling: ', results_rand.acc)
+        results_super_class = anlz.analyze_clustering(corpus.super_class_labels[:param.max_num], clusters,
+                                                      number_of_labels)
+        # print('accuracy: ', results.acc)
+        # print('accuracy random labeling: ', results_rand.acc)
         print('analyzing and saving...')
 
-        acc = save_results(param,results)
+        acc = save_results(param, results)
         print('accuracy = %f' % results.get_list()[0])
         print('accuracy by super_class= %f' % results_super_class.get_list()[0])
         '''
@@ -186,14 +188,14 @@ for idx,param in enumerate(prarameters):
         plt.imshow(results.conf_mat[0:number_of_labels-1,:])
         #plt.colorbar()
         plt.savefig('results/' + timestamp + '/conf_mat.png')
-    
+
         plt.figure(2,figsize=(20,20))
         plt.imshow(results_rand.conf_mat[0:number_of_labels-1,:])
         #plt.colorbar()
-    
+
         plt.savefig('results/' + timestamp + '/conf_mat_rand.png')
-    
-    
+
+
         plt.figure(4)
         plt.plot(range(1, 1 + number_of_labels), results.precision, '*r')
         plt.plot(range(1, 1 + number_of_labels), results_rand.precision, '*')
@@ -201,7 +203,7 @@ for idx,param in enumerate(prarameters):
         plt.ylim([0, 1])
         plt.title('precision')
         plt.savefig('results/' + timestamp + '/precision.png')
-    
+
         plt.figure(5)
         plt.plot(range(1, 1 + number_of_labels), results.recall, '*r')
         plt.plot(range(1, 1 + number_of_labels), results_rand.recall, '*')
@@ -209,17 +211,16 @@ for idx,param in enumerate(prarameters):
         plt.ylim([0, 1])
         plt.title('recall')
         plt.savefig('results/' + timestamp + '/recall.png')
-    
+
         #labels histogram
-    
+
         plt.figure(4)
         plt.hist(labels,rwidth=0.85,bins = range(21) )
         plt.xticks(range(20))
         plt.show()
         '''
     except Exception as e:
-        print('error: %s' %  e)
+        print('error: %s' % e)
         raise
 
-
-#total_linkage_matrix = anlz.calc_total_linkage_matrix(num_of_documents)
+# total_linkage_matrix = anlz.calc_total_linkage_matrix(num_of_documents)
